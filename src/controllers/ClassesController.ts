e9fc933229b4e1e1db13fdd06b7f21755ec60e4e
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2020-08-06 17:25:51 -0300

    Fix do bug do cors, reset do database

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -11,30 +11,31 @@ interface ScheduleItem {
 export default class ClassesController {
     async index(request: Request, response: Response) {
         const filters = request.query;
-        const subject = filters.subject as string
-        const week_day = filters.week_day as string
-        const time = filters.time as string
-
+        
         if(!filters.week_day || !filters.subject || !filters.time) {
             return response.status(400).json({
                 error: "Missing filters to search classes"
             })
         }
 
+        const subject = filters.subject as string
+        const week_day = filters.week_day as string
+        const time = filters.time as string
+
         const timeInMinutes = convertHourToMinutes(time)
         
         const classes = await db('classes')
-            .whereExists(function () {
-                this.select('class_schedule.*')
-                    .from('class_schedule')
-                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
-            })
-            .where('classes.subject', '=', subject)
-            .join('users', 'classes.user_id', '=', 'user_id')
-            .select(['classes.*', 'users.*']);
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
 
         return response.json(classes);
     }
@@ -57,44 +58,42 @@ export default class ClassesController {
         const trx = await db.transaction();
     
         try {
-            const insertedUsersIds = await trx('users').insert({
-                name,
-                avatar,
-                whatsapp,
-                bio,
+            const UsersId = await trx('users').insert({
+              name,
+              avatar,
+              whatsapp,
+              bio,
             });
-        
-            const user_id = insertedUsersIds[0]
-        
-            const insertedClassesIds = await trx('classes').insert({
-                subject,
-                cost,
-                user_id
-            })
-        
-            const class_id = insertedClassesIds[0];
-        
+      
+            const user_id = UsersId[0];
+      
+            const classesId = await trx('classes').insert({
+              subject,
+              cost,
+              user_id,
+            });
+      
+            const class_id = classesId[0];
+      
             const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-                return {
-                    week_day: scheduleItem.week_day,
-                    from: convertHourToMinutes(scheduleItem.from),
-                    to: convertHourToMinutes(scheduleItem.to),
-                    class_id
-                };
-            })
-        
-            await trx('class_schedule').insert(classSchedule)
-        
+              const from = convertHourToMinutes(scheduleItem.from);
+      
+              const to = convertHourToMinutes(scheduleItem.to);
+      
+              return { class_id, week_day: scheduleItem.week_day, from, to };
+            });
+      
+            await trx('class_schedule').insert(classSchedule);
+      
             await trx.commit();
-        
+      
             return response.status(201).send();
-        
-        } catch(err) {
+          } catch (error) {
+            // desfazer transações feitas antes do erro.
             await trx.rollback();
-            console.log(err)
-            return response.status(400).json({
-                error: 'Unexpected error while creating new class'
-            })
+            return response
+              .status(400)
+              .json({ error: 'Erro inesperado ao criar uma nova classe.' });
+          }
         }
-    }
-}
+      }
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import cors from 'cors';
 const app = express();
 // O express normalmente não entende o formato json. Devemos fazer esta chamada abaixo para que 
 // ele passe a converter os dados das requisições para json.
+app.use(cors())
 app.use(express.json());
 app.use(routes)
-app.use(cors)
 
 //localhost:3333
 app.listen(3333);
